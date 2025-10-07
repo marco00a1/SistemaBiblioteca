97c442d69f8baf07812edf51d630b6450c48a3b6
mneciosup <marconeciosup13@gmail.com>
2025-10-07 11:46:02 -0500

    Prueba de commit 07/10/2025

--- src/BibliotecaApp.java
+++ src/BibliotecaApp.java
@@ -1,5 +1,7 @@
 import vista.LoginFrame;
 
+/* Prueba de commit.*/
+
 public class BibliotecaApp {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
